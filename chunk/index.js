188999d60fc0d8d6dc49c97f9dd80dd5ecfd3b5e
Parveen Arora <parveen.arora@jabong.com>
2018-12-27 02:46:12 +0530

    UPDATE: Added Chunk Programs

---
+++ chunk/index.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+    let arr = [], tmp = [];
+    for (let i=0; i < array.length; i++) {
+        if (tmp.length === size) {
+            arr.push(tmp);
+            tmp = [];
+        }
+        tmp.push(array[i]);
+    }
+    if (tmp.length) {
+        arr.push(tmp);
+    }
+    return arr;
+}
+
+module.exports = chunk;
